4ae0b2bfc9e30b0488ac55baf43388f1a4a76253
juliatrue <trju24cc@student.ju.se>
2025-03-10 20:43:50 +0100

    adding dune and babel to shopping cart

--- js/main.js
+++ js/main.js
@@ -1,61 +1,122 @@
-//let books = [];
-//let booksJson = "none";
-let books = 0;
-let count = 0;
+let books = JSON.parse(localStorage.bookInCart || "[]");
+let babels = JSON.parse(localStorage.babels || "[]");
 
-if (document.getElementById("add-cart")) {
-  const duneButton = document.getElementById("add-cart");
+// DUNE
+if (
+  document.getElementById("add-cart") ||
+  document.getElementById("dune-counter")
+) {
+  if (document.getElementById("add-cart")) {
+    const duneButton = document.getElementById("add-cart");
 
-  duneButton.addEventListener("click", function () {
-    books += 1;
-    localStorage.dune = books;
+    duneButton.addEventListener("click", function () {
+      books.push("Dune");
 
-    /*
-    books.push("Dune");
-    booksJson = JSON.stringify(books);
+      localStorage.bookInCart = JSON.stringify(books);
+    });
+  }
 
-    localStorage.bookInCart = booksJson;
-    */
-  });
+  if (document.getElementById("dune-counter")) {
+    const duneCounter = document.getElementById("dune-counter");
+    const duneAdd = document.getElementById("dune-add-cart");
+    const duneDelete = document.getElementById("dune-delete-cart");
+    const duneSection = document.getElementById("dune-display");
+    const dunePrice = document.getElementById("dune-price");
+
+    let currentAmount = JSON.parse(localStorage.bookInCart);
+    duneCounter.innerText = currentAmount.length;
+
+    dunePrice.innerText = 130 * currentAmount.length + " kr";
+
+    if (currentAmount.length > 0) {
+      duneSection.style.display = "flex";
+    }
+
+    duneAdd.addEventListener("click", function () {
+      currentAmount.push("Dune");
+      localStorage.bookInCart = JSON.stringify(currentAmount);
+
+      let dunes = JSON.parse(localStorage.bookInCart);
+      duneCounter.innerText = dunes.length;
+      dunePrice.innerText = 130 * currentAmount.length + " kr";
+    });
+
+    duneDelete.addEventListener("click", function () {
+      if (duneCounter.innerText > 0) {
+        currentAmount.pop();
+        localStorage.bookInCart = JSON.stringify(currentAmount);
+
+        let dunes = JSON.parse(localStorage.bookInCart);
+        duneCounter.innerText = dunes.length;
+        dunePrice.innerText = 130 * currentAmount.length + " kr";
+      }
+
+      let dunes = JSON.parse(localStorage.bookInCart);
+
+      if (dunes.length === 0) {
+        duneSection.style.display = "none";
+      }
+    });
+  }
 }
 
-if (document.getElementById("dune-counter")) {
-  const duneCounter = document.getElementById("dune-counter");
-  const duneAdd = document.getElementById("dune-add-cart");
-  const duneDelete = document.getElementById("dune-delete-cart");
+// BABEL
+if (
+  document.getElementById("buy-babel") ||
+  document.getElementById("babel-counter")
+) {
+  if (document.getElementById("buy-babel")) {
+    const babelAdd = document.getElementById("buy-babel");
 
-  localStorage.dune = books;
+    babelAdd.addEventListener("click", function () {
+      babels.push("Babel");
 
-  duneCounter.innerText = localStorage.dune;
+      localStorage.babels = JSON.stringify(babels);
+    });
+  }
 
-  duneAdd.addEventListener("click", function () {
-    books += 1;
+  if (document.getElementById("babel-counter")) {
+    const babelCounter = document.getElementById("babel-counter");
+    const babelAdd = document.getElementById("babel-add-cart");
+    const babelDelete = document.getElementById("babel-delete-cart");
+    const babelSection = document.getElementById("babel-display");
+    const babelPrice = document.getElementById("babel-price");
 
-    /*
-    console.log(count);
-    duneCounter.innerText = bookTest.length + count;
-    */
-  });
+    let babelAmount = JSON.parse(localStorage.babels);
+    babelCounter.innerText = babelAmount.length;
 
-  duneDelete.addEventListener("click", function () {
-    if (duneCounter.innerText > 0) {
-      count -= 1;
-      console.log(count);
-      duneCounter.innerText = bookTest.length + count;
+    babelPrice.innerText = 130 * babelAmount.length + " kr";
+
+    if (babelAmount.length > 0) {
+      babelSection.style.display = "flex";
     }
-  });
+
+    babelAdd.addEventListener("click", function () {
+      babelAmount.push("Babel");
+      localStorage.babels = JSON.stringify(babelAmount);
+
+      let babelNumber = JSON.parse(localStorage.babels);
+      babelCounter.innerText = babelNumber.length;
+      babelPrice.innerText = 130 * babelAmount.length + " kr";
+    });
+
+    babelDelete.addEventListener("click", function () {
+      if (babelCounter.innerText > 0) {
+        babelAmount.pop();
+        localStorage.babels = JSON.stringify(babelAmount);
+
+        let babelNumber = JSON.parse(localStorage.babels);
+        babelCounter.innerText = babelNumber.length;
+        babelPrice.innerText = 130 * babelAmount.length + " kr";
+      }
+
+      let babelNumber = JSON.parse(localStorage.babels);
+
+      if (babelNumber.length === 0) {
+        babelSection.style.display = "none";
+      }
+    });
+  }
 }
 
 //localStorage.clear();
-
-//console.log(books);
-
-/*
-console.log(bookTest);
-
-console.log(bookTest.length);
-*/
-
-//let book = localStorage.bookInCart;
-
-//let bookTest = JSON.parse(localStorage.bookInCart);
